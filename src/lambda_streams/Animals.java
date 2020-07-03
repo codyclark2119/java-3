525ba6014bfbbfbfd3e6da432bf4642d6b5721d7
Cody Clark <codyclark2119@gmail.com>
2020-07-02 23:59:06 -0500

    Finished Lambda refactoring

--- src/lambda_streams/Animals.java
+++ src/lambda_streams/Animals.java
@@ -8,96 +8,95 @@ import java.util.stream.Stream;
 
 class Animals {
     //This class will be using streams.
-    static List<String> animals = Arrays.asList("peacoCK","rabbit","chiwawa","OranguTAN","vipeR","cobra","paNDa","bUffalo","DeeR","maLLard");
+    static List<String> animals = Arrays.asList("peacoCK", "rabbit", "chiwawa", "OranguTAN", "vipeR", "cobra", "paNDa",
+            "bUffalo", "DeeR", "maLLard");
 
     public static void main(String[] args) {
-        System.out.println("original animals : " + animals);
 
         //clean up the animals array by using the capsFirst() method. follow instructions in the method definition. 
-        List<String> cleaned = capsFirst(animals, false);
-        System.out.println(cleaned);
-       
+        lambdaList capsFirst = (animaList, mutate) -> {
+            if (mutate) {
+                animals = animaList.stream().map(String::toLowerCase)
+                        .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                        .collect(Collectors.toList());
+                return animals;
+            } else {
+                return animaList.stream().map(String::toLowerCase)
+                        .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            }
+        };
+
+        lambdaList lowerFirst = (animaList, mutate) -> {
+            if (mutate) {
+                animals = animaList.stream().map(String::toUpperCase)
+                        .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
+                        .collect(Collectors.toList());
+                return animals;
+            } else {
+                return animaList.stream().map(String::toUpperCase)
+                        .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            }
+        };
         //do not modify these addAnimal() method invocations
-        addAnimal("rEIndeeR");
-        addAnimal("Platypus");
-        addAnimal("frOg");
-        addAnimal("lEOpArD");
+        lambdaString addAnimal = (animal) -> {
+            //add an animal to the animal list.
+            Stream<String> concatStream = Stream.concat(animals.stream(), Stream.of(animal));
+            animals = concatStream.collect(Collectors.toList());
+            return animal;
+        };
+
+        lambdaBoolean flipAnimals = (mutate) -> {
+            if (mutate) {
+                Collections.reverse(animals);
+                return animals;
+            } else {
+                Stream<String> flipStream = animals.stream();
+                List<String> flipList = flipStream.collect(Collectors.toList());
+                Collections.reverse(flipList);
+                return flipList;
+            }
+        };
+
+        lambdaBoolean sortAnimals = (mutate) -> {
+            if (mutate) {
+                Collections.sort(animals);
+                return animals;
+            } else {
+                Stream<String> sortStream = animals.stream();
+                List<String> sortList = sortStream.collect(Collectors.toList());
+                Collections.sort(sortList);
+                return sortList;
+            }
+        };
+
+        System.out.println("Original Animals: " + animals);
+        System.out.println("Caps First No Modify: " + capsFirst.returnList(animals, false));
+        System.out.println("Animals: " + animals);
+        addAnimal.returnString("rEIndeeR");
+        addAnimal.returnString("Platypus");
+        addAnimal.returnString("frOg");
+        addAnimal.returnString("lEOpArD");
         //---------------------------------------
-
-        capsFirst(animals,true);
-        System.out.println(animals);
-
-        List<String> lowered = lowerFirst(animals,false);
-        System.out.println(lowered);
-        System.out.println(sortAnimals(true));
-        System.out.println(flipAnimals(false));
+        System.out.println("Caps First Modify: " + capsFirst.returnList(animals, true));
+        System.out.println("Lower First No Modify: " + lowerFirst.returnList(animals, false));
+        System.out.println("Sort Modify: " + sortAnimals.returnNewList(true));
+        System.out.println("Flip No Modify: " + flipAnimals.returnNewList(false));
 
     }
 
-    static List<String> capsFirst(List<String> animaList, boolean mutate) {
-        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.  Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate' the original animals array stored as a static class field.  if the flag is set to 'true', mutate the animals and return the animals out of the function.  if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function, WITHOUT modifying the original animals array.  
-        if (mutate) {
-            animals = animaList.stream().map(String::toLowerCase)
-                    .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
-                    .collect(Collectors.toList());
-            return animals;
-        } else {
-            return animaList.stream().map(String::toLowerCase)
-                    .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
-                    .collect(Collectors.toList());
-        }
+    interface lambdaList {
+        //A method with single parameter
+        public List<String> returnList(List<String> animaList, boolean mutate);
     }
 
-    static String addAnimal(String animal) {
-        //add an animal to the animal list.
-        Stream<String> concatStream = Stream.concat(animals.stream(), Stream.of(animal));
-        animals = concatStream.collect(Collectors.toList());
-        return animal;
-    };
-
-
-    static List<String> lowerFirst(List<String> animaList, boolean mutate) {
-        //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on a 'copy' of the animals list.  return the list out of hte function in both cases.
-        if (mutate) {
-            animals = animaList.stream().map(String::toUpperCase)
-                    .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
-                    .collect(Collectors.toList());
-            return animals;
-        } else {
-            return animaList.stream().map(String::toUpperCase)
-                    .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
-                    .collect(Collectors.toList());
-        }
+    interface lambdaBoolean {
+        public List<String> returnNewList(boolean mutate);
     }
 
-    static List<String> flipAnimals(boolean mutate) {
-        //reverse the order of the animals in the animal list.  If the booleaen parameter is true, reverse the static animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the flipped list in both cases.
-        List<String> instructions = Arrays.asList("Flip","the","animals","list","."," ","Mutate","the","animals","only","if","flag","true");
-        if (mutate) {
-            Collections.reverse(animals);
-            return animals;
-        } else {
-            Stream<String> flipStream = animals.stream();
-            List<String> flipList = flipStream.collect(Collectors.toList());
-            Collections.reverse(flipList);
-            return flipList;
-        }
-
+    interface lambdaString {
+        public String returnString(String animal);
     }
-
-    static List<String> sortAnimals(boolean mutate) {
-    //sort the animals in alphabetical order.  If the booleaen parameter is true, mutating the animals list.  if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals, WITHOUT mutating the static animals array. return the sorted list in both cases.
-        if (mutate) {
-            Collections.sort(animals);
-            return animals;
-        } else {
-            Stream<String> sortStream = animals.stream();
-            List<String> sortList = sortStream.collect(Collectors.toList());
-            Collections.sort(sortList);
-            return sortList;
-        }
-
-    }
-
 
 }
--- src/lambda_streams/Numbers.java
+++ src/lambda_streams/Numbers.java
@@ -7,166 +7,173 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Numbers {
-    static List<Integer> nums = Arrays.asList(10,100,1000,5,50,500,3,30,300,7,70,700,1,10,100,25,250,2500);
+    static List<Integer> nums = Arrays.asList(10, 100, 1000, 5, 50, 500, 3, 30, 300, 7, 70, 700, 1, 10, 100, 25, 250,
+            2500);
+
     public static void main(String[] args) {
         //Part I :complete the static class methods that have been set up in this Numbers class java file.  Use streams to compute the results wheever possible.
-        System.out.println(isOdd(1));
-        System.out.println(isEven(2));
-        System.out.println(isPrime(0));
-        System.out.println(added());
-        System.out.println(subtracted());
-        System.out.println(multipled());
-        System.out.println(divided());
-        System.out.println(findMax());
-        System.out.println(findMin());
-        System.out.println(compare(1,0));
-        System.out.println(append(12));
-        //Part II - refactor all of the class methods to accept lambda expressions. You can put the lambda functions directly inside the method calls, or defined them first, then pass them into the methods. give them the same names as the static methods, but add the word 'lambda' in front of every lambda function:
-        /* e.g.
-
-        added(() -> {});
-
-        OR
-
-        lambdaAdd = () -> {};
-        added(lambdaAdd);
-
-        isOdd(() -> {});
-
-        OR
-
-        lambdaOdd = () -> {};
-        isOdd(lambdaOdd);
-        etc...
-
-        */
-
-
-    }
-
-
-    static boolean isOdd(int i) {
-        //determine if the value at the index i is odd.  return true if yes, return false if  no.
-        System.out.println("isOdd:");
-        Integer num = nums.get(i);
-        String[] numArr = Integer.toString(num).split("");
-        if (numArr[numArr.length - 1].equals("1") || numArr[numArr.length - 1].equals("3") || numArr[numArr.length - 1].equals("5")|| numArr[numArr.length - 1].equals("7") || numArr[numArr.length - 1].equals("9") ){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    static boolean isEven(int i) {
-        //determine if the value at the index i is even.  return true if yes, return false if  no.
-        System.out.println("isEven:");
-        Integer num = nums.get(i);
-        String[] numArr = Integer.toString(num).split("");
-        if (numArr[numArr.length - 1].equals("0") || numArr[numArr.length - 1].equals("2") || numArr[numArr.length - 1].equals("4")|| numArr[numArr.length - 1].equals("6") || numArr[numArr.length - 1].equals("8") ){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    static boolean isPrime(int i) {
-         //determine if the value at the index i is a prime number.  return true if yes, return false if no.
-        System.out.println("isPrime:");
-        Integer num = nums.get(i);
-        if(num%2 == 0) return false;
-        for(int n=3; n*n<=num; n+=2){
-            if(num%n==0)
+        lambdaBoolean isOdd = (i) -> {
+            //determine if the value at the index i is odd.  return true if yes, return false if  no.
+            System.out.println("isOdd:");
+            Integer num = nums.get(i);
+            String[] numArr = Integer.toString(num).split("");
+            if (numArr[numArr.length - 1].equals("1") || numArr[numArr.length - 1].equals("3")
+                    || numArr[numArr.length - 1].equals("5") || numArr[numArr.length - 1].equals("7")
+                    || numArr[numArr.length - 1].equals("9")) {
+                return true;
+            } else {
                 return false;
-        }
-        return true;
+            }
+        };
+        System.out.println(isOdd.returnBoolean(0));
+
+        lambdaBoolean isEven = (i) -> {
+            //determine if the value at the index i is even.  return true if yes, return false if  no.
+            System.out.println("isEven:");
+            Integer num = nums.get(i);
+            String[] numArr = Integer.toString(num).split("");
+            if (numArr[numArr.length - 1].equals("0") || numArr[numArr.length - 1].equals("2")
+                    || numArr[numArr.length - 1].equals("4") || numArr[numArr.length - 1].equals("6")
+                    || numArr[numArr.length - 1].equals("8")) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+        System.out.println(isEven.returnBoolean(2));
+
+        lambdaBoolean isPrime = (i) -> {
+            //determine if the value at the index i is a prime number.  return true if yes, return false if no.
+            System.out.println("isPrime:");
+            Integer num = nums.get(i);
+            if (num % 2 == 0)
+                return false;
+            for (int n = 3; n * n <= num; n += 2) {
+                if (num % n == 0)
+                    return false;
+            }
+            return true;
+        };
+        System.out.println(isPrime.returnBoolean(0));
+
+        lambdaInt added = () -> {
+            //add all the elements in the list.  return the sum.
+            System.out.println("added:");
+            Stream<Integer> numStream = nums.stream();
+            Optional<Integer> reduced = numStream.reduce((value, combinedValue) -> {
+                return combinedValue + value;
+            });
+            return reduced.get();
+        };
+        System.out.println(added.returnInt());
+
+        lambdaInt subtracted = () -> {
+            //subtract all the elements in the list. return the remainder.
+            System.out.println("subtracted:");
+            Stream<Integer> numStream = nums.stream();
+            Optional<Integer> reduced = numStream.reduce((value, combinedValue) -> {
+                return combinedValue - value;
+            });
+            return reduced.get();
+        };
+        System.out.println(subtracted.returnInt());
+
+        lambdaLong multipled = () -> {
+            System.out.println("multipled:");
+            List<Long> longNums = nums.stream().mapToLong(Integer::longValue).boxed().collect(Collectors.toList());
+            Stream<Long> numStream = longNums.stream();
+            Optional<Long> reduced = numStream.reduce((value, combinedValue) -> {
+                return combinedValue * value;
+            });
+            return reduced.get();
+        };
+        System.out.println(multipled.returnLong());
+
+        lambdaDouble divided = () -> {
+            System.out.println("divided:");
+            //multiply all the elements in the list. and return the product.
+            List<Double> dubNums = nums.stream().mapToDouble(Integer::doubleValue).boxed().collect(Collectors.toList());
+            Stream<Double> numStream = dubNums.stream();
+            Optional<Double> reduced = numStream.reduce((value, combinedValue) -> {
+                return value / combinedValue;
+            });
+            return reduced.get();
+        };
+        System.out.println(divided.returnDouble());
+
+        lambdaInt findMax = () -> {
+            System.out.println("findMax:");
+            Stream<Integer> numStream = nums.stream();
+            Optional<Integer> max = numStream.max((val1, val2) -> {
+                return val1.compareTo(val2);
+            });
+            return max.get();
+        };
+        System.out.println(findMax.returnInt());
+
+        lambdaInt findMin = () -> {
+            System.out.println("findMin:");
+            Stream<Integer> numStream = nums.stream();
+            Optional<Integer> min = numStream.min((val1, val2) -> {
+                return val1.compareTo(val2);
+            });
+            return min.get();
+        };
+        System.out.println(findMin.returnInt());
+
+        lambdaCompare compare = (i, j) -> {
+            System.out.println("compare:");
+            //compare the values stored in the array at index position i and j.  
+            //if the value at i is greater, return 1.  if the value at j is greater, return -1.  if the two values are equal, return 0.
+            if (i > j) {
+                return 1;
+            } else if (i == j) {
+                return 0;
+            } else {
+                return -1;
+            }
+        };
+        System.out.println(compare.returnCompare(1, 0));
+
+        lambdaAppend append = (i) -> {
+            System.out.println("append:");
+            //add a new value to the values list. return that value after adding it to the list.
+            System.out.println("Original array size: " + nums.size());
+
+            Stream<Integer> concatStream = Stream.concat(nums.stream(), Stream.of(i));
+
+            List<Integer> newNums = concatStream.collect(Collectors.toList());
+            nums = newNums;
+            System.out.println("New array size: " + nums.size());
+            return i;
+        };
+        System.out.println(append.returnAppend(12));
+
     }
 
-    static int added() {
-        //add all the elements in the list.  return the sum.
-        System.out.println("added:");
-        Stream<Integer> numStream = nums.stream();
-        Optional<Integer> reduced = numStream.reduce((value, combinedValue) -> {
-            return combinedValue + value;
-        });
-        return reduced.get();
+    interface lambdaBoolean {
+        //A method with single parameter
+        public boolean returnBoolean(int i);
     }
 
-    static int subtracted() {
-        //subtract all the elements in the list. return the remainder.
-        System.out.println("subtracted:");
-        Stream<Integer> numStream = nums.stream();
-        Optional<Integer> reduced = numStream.reduce((value, combinedValue) -> {
-            return combinedValue - value;
-        });
-        return reduced.get();
+    interface lambdaInt {
+        //A method with single parameter
+        public int returnInt();
     }
 
-    static Long multipled() {
-        System.out.println("multipled:");
-        List<Long> longNums = nums.stream()
-                .mapToLong(Integer::longValue)
-                .boxed()
-                .collect(Collectors.toList());
-        Stream<Long> numStream = longNums.stream();
-        Optional<Long> reduced = numStream.reduce((value, combinedValue) -> {
-            return combinedValue * value;
-        });
-        return reduced.get();
+    interface lambdaLong {
+        public long returnLong();
     }
 
-    static Double divided() {
-        System.out.println("divided:");
-        //multiply all the elements in the list. and return the product.
-        List<Double> dubNums = nums.stream()
-                .mapToDouble(Integer::doubleValue)
-                .boxed()
-                .collect(Collectors.toList());
-        Stream<Double> numStream = dubNums.stream();
-        Optional<Double> reduced = numStream.reduce((value, combinedValue) -> {
-            return value / combinedValue;
-        });
-        return reduced.get();
+    interface lambdaDouble {
+        public double returnDouble();
     }
 
-    static int findMax() {
-        System.out.println("findMax:");
-        Stream<Integer> numStream = nums.stream();
-        Optional<Integer> max = numStream.max((val1, val2) -> {
-            return val1.compareTo(val2);
-        });
-        return max.get();
+    interface lambdaCompare {
+        public int returnCompare(int i, int j);
     }
 
-    static int findMin() {
-        System.out.println("findMin:");
-        Stream<Integer> numStream = nums.stream();
-        Optional<Integer> min = numStream.min((val1, val2) -> {
-            return val1.compareTo(val2);
-        });
-        return min.get();
+    interface lambdaAppend {
+        public int returnAppend(int i);
     }
-
-    static int compare(int i, int j) {
-        System.out.println("compare:");
-        //compare the values stored in the array at index position i and j.  
-        //if the value at i is greater, return 1.  if the value at j is greater, return -1.  if the two values are equal, return 0.
-        if (i > j){return 1;}
-        else if (i == j){ return 0;}
-        else{return -1;}
-    }
-
-    static int append(int n) {
-        System.out.println("append:");
-        //add a new value to the values list. return that value after adding it to the list.
-        System.out.println("Original array size: " + nums.size());
-
-        Stream<Integer> concatStream = Stream.concat(nums.stream(), Stream.of(n));
-
-        List<Integer> newNums = concatStream
-                .collect(Collectors.toList());
-        nums = newNums;
-        System.out.println("New array size: " + nums.size());
-        return n;
-    }
-
 }
